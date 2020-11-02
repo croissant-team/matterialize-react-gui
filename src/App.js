c466a7c2ce711b6fc93b71134db7ddf7d1682296
Raihaan Rahman <raihaan2000@gmail.com>
2020-11-02 11:09:27 +0000

    Added basic webcam select to the gui

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import WebcamCapture from "./components/WebcamCapture";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,9 +19,11 @@ function App() {
         >
           Learn React
         </a>
-        <WebcamCapture />
+        
         <Button color="primary">Hello World</Button>
-      </header>
+      </header> */}
+
+      <WebcamCapture />
 
     </div>
   );
--- src/components/WebcamCapture.jsx
+++ src/components/WebcamCapture.jsx
@@ -1,35 +1,47 @@
 import React from "react";
 import Webcam from "react-webcam";
+import { InputLabel, Select, MenuItem } from '@material-ui/core';
 
 const WebcamCapture = () => {
-   const [deviceId, setDeviceId] = React.useState({});
-   const [devices, setDevices] = React.useState([]);
-  
-   const handleDevices = React.useCallback(
-     mediaDevices =>
-       setDevices(mediaDevices.filter(({ kind }) => kind === "videoinput")),
-     [setDevices]
-   );
-  
-   React.useEffect(
-     () => {
-       navigator.mediaDevices.enumerateDevices().then(handleDevices);
-     },
-     [handleDevices]
-   );
-  
-   return (
-     <>
-       {devices.map((device, key) => (
-           <div>
-             <Webcam audio={false} videoConstraints={{ deviceId: device.deviceId }} />
-             {device.label || `Device ${key + 1}`}
-           </div>
-  
-         ))}
-     </>
-   );
- };
+  const [deviceId, setDeviceId] = React.useState({});
+  // const [deviceName, setDeviceName] = React.useState("Select a webcam");
+  const [devices, setDevices] = React.useState([]);
+
+  const setWebcam = (id, name) => {
+    setDeviceId(id);
+    // setDeviceName(name);
+  }
+
+  const handleChange = (event) => {
+    setWebcam(event.target.value);
+  };
+
+  const handleDevices = React.useCallback(
+    mediaDevices =>
+      setDevices(mediaDevices.filter(({ kind }) => kind === "videoinput")),
+    [setDevices]
+  );
+
+  React.useEffect(
+    () => {
+      navigator.mediaDevices.enumerateDevices().then(handleDevices);
+    },
+    [handleDevices]
+  );
+
+  return (
+    <>
+      <Webcam width={640} height={480} mirrored audio={false} videoConstraints={{ deviceId: deviceId}} />
+
+      <InputLabel color="primary" id="label">Select a webcam</InputLabel>
+      <Select labelId="label" id="select" value={deviceId} onChange={handleChange}>
+        {devices.map((device, key) => (
+          <MenuItem value={device.deviceId}>{device.label || `Device ${key + 1}`}</MenuItem>
+        ))}
+      </Select>
+    </>
+  );
+};
 
 
 export default WebcamCapture;
