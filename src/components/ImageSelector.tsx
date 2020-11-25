37a4a7b5eb790087d0f126fc1f33ecc75532ffd7
Raihaan Rahman <raihaan2000@gmail.com>
2020-11-25 16:25:23 +0000

    Added checks to see if a file is a video or an image

--- src/components/ImageSelector.tsx
+++ src/components/ImageSelector.tsx
@@ -6,11 +6,20 @@ const ImageSelector: React.FC = () => {
 
   const imgInputRef = React.useRef<HTMLInputElement>(null)
 
-  const handleChange = (path: string): void => {
-    fetch('http://localhost:9000/background/set', {
-      method: 'POST',
-      body: JSON.stringify({ file_path: path }),
-    })
+  const handleChange = (file: File): void => {
+    const path = (file as any).path
+
+    if (file.type.includes("video")) {
+      console.log(file.type)
+    } 
+
+    if (file.type.includes("image")) {  
+      fetch('http://localhost:9000/background/set', {
+        method: 'POST',
+        body: JSON.stringify({ file_path: path }),
+      })
+    }
+
   }
 
   return (
@@ -22,9 +31,9 @@ const ImageSelector: React.FC = () => {
         ref={imgInputRef}
         hidden
         type="file"
-        accept="image/*"
+        accept="image/* video/*"
         onChange={e => {
-          handleChange(((e.nativeEvent.target as HTMLInputElement).files![0] as any).path)
+          handleChange(((e.nativeEvent.target as HTMLInputElement).files![0] as File))
         }}
       />
     </>
